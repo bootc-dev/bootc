e4e0339d6ac2043319aa2ac03763cd59a8cf3c7f
Omer Tuchfeld <omer@tuchfeld.dev>
2025-04-17 09:36:24 +0200

    ocpver

--- build_seed.sh
+++ build_seed.sh
@@ -7,5 +7,5 @@ set -euxo pipefail
 cd $SCRIPT_DIR
 
 podman build -t bootcseed -f Containerfile .
-podman tag bootcseed:latest quay.io/otuchfel/bootc:seed6
-podman push quay.io/otuchfel/bootc:seed6
+podman tag bootcseed:latest quay.io/otuchfel/bootc:seed61
+podman push quay.io/otuchfel/bootc:seed61
