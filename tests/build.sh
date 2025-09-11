a6d6ebde473f19fcf56ea88cc6da069b003e1d7e
Colin Walters <walters@verbum.org>
2025-09-11 13:57:46 -0400

    ci: Unify more of hack/ and tests/
    A key thing for me is that the `Justfile` should be a one-stop
    shop for development of the project. It can't have everything but
    it should answer the basic questions of "how do I build and test
    this project".
    
    This aligns the recently added tmt-on-GHA flow a *bit* more closely
    with some of that. Biggest is to use the `just build-integration-test-image` as the canonical
    way to build a container image with our testing stuff in it;
    which uses our main Dockerfile
    
    Other cleanups:
    - Change the scripts to accept data via argv[1] and not environment
    - Drop the hardcoded testing directory and use `target/` as
      a generic build artifact dir
    
    Signed-off-by: Colin Walters <walters@verbum.org>

--- tests/build.sh
+++ tests/build.sh
@@ -4,101 +4,51 @@ set -exuo pipefail
 # This script basically builds bootc from source using the provided base image,
 # then runs the target tests.
 
-mkdir -p /tmp/tmp-bootc-build
-BOOTC_TEMPDIR="/tmp/tmp-bootc-build"
+# If provided should be of the form fedora-42 or centos-10
+target=${1:-}
 
-# Get OS info from TEST_OS env
-OS_ID=$(echo "$TEST_OS" | cut -d '-' -f 1)
-OS_VERSION_ID=$(echo "$TEST_OS" | cut -d '-' -f 2)
+build_args=()
+if test -n "${target:-}"; then
+    shift
+    # Get OS info from TEST_OS env
+    OS_ID=$(echo "$target" | cut -d '-' -f 1)
+    OS_VERSION_ID=$(echo "$target" | cut -d '-' -f 2)
 
-# Base image
-case "$OS_ID" in
-    "centos")
-        TIER1_IMAGE_URL="quay.io/centos-bootc/centos-bootc:stream${OS_VERSION_ID}"
+    # Base image
+    case "$OS_ID" in
+        "centos")
+            BASE="quay.io/centos-bootc/centos-bootc:stream${OS_VERSION_ID}"
         ;;
-    "fedora")
-        TIER1_IMAGE_URL="quay.io/fedora/fedora-bootc:${OS_VERSION_ID}"
+        "fedora")
+            BASE="quay.io/fedora/fedora-bootc:${OS_VERSION_ID}"
         ;;
-esac
+        *) echo "Unknown OS: ${OS_ID}" 1>&2; exit 1
+        ;;
+    esac
+    build_args+=("--build-arg=base=$BASE")
+fi
 
-CONTAINERFILE="${BOOTC_TEMPDIR}/Containerfile"
-tee "$CONTAINERFILE" > /dev/null << CONTAINERFILEOF
-FROM $TIER1_IMAGE_URL as build
+just build ${build_args[@]}
+just build-integration-test-image
 
-WORKDIR /code
+# Host builds will have this already, but we use it as a general dumping space
+# for output artifacts
+mkdir -p target
 
-RUN <<EORUN
-set -xeuo pipefail
-. /usr/lib/os-release
-case \$ID in
-    centos|rhel) dnf config-manager --set-enabled crb;;
-    fedora) dnf -y install dnf-utils 'dnf5-command(builddep)';;
-esac
-dnf -y builddep contrib/packaging/bootc.spec
-dnf -y install git-core
-EORUN
-
-RUN mkdir -p /build/target/dev-rootfs
-# git config --global --add safe.directory /code to fix "fatal: detected dubious ownership in repository at '/code'" error
-RUN --mount=type=cache,target=/build/target --mount=type=cache,target=/var/roothome git config --global --add safe.directory /code && make test-bin-archive && mkdir -p /out && cp target/bootc.tar.zst /out
-
-FROM $TIER1_IMAGE_URL
-
-# Inject our built code
-COPY --from=build /out/bootc.tar.zst /tmp
-RUN tar -C / --zstd -xvf /tmp/bootc.tar.zst && rm -vrf /tmp/*
-
-RUN <<EORUN
-set -xeuo pipefail
-
-# Provision test requirement
-/code/hack/provision-derived.sh
-# Also copy in some default install configs we use for testing
-cp -a /code/hack/install-test-configs/* /usr/lib/bootc/install/
-# And some test kargs
-cp -a /code/hack/test-kargs/* /usr/lib/bootc/kargs.d/
-
-# For testing farm
-mkdir -p -m 0700 /var/roothome
-
-# Enable ttyS0 console
-mkdir -p /usr/lib/bootc/kargs.d/
-cat <<KARGEOF >> /usr/lib/bootc/kargs.d/20-console.toml
-kargs = ["console=ttyS0,115200n8"]
-KARGEOF
-
-# For test-22-logically-bound-install
-cp -a /code/tmt/tests/lbi/usr/. /usr
-ln -s /usr/share/containers/systemd/curl.container /usr/lib/bootc/bound-images.d/curl.container
-ln -s /usr/share/containers/systemd/curl-base.image /usr/lib/bootc/bound-images.d/curl-base.image
-ln -s /usr/share/containers/systemd/podman.image /usr/lib/bootc/bound-images.d/podman.image
-
-# Install rsync which is required by tmt
-dnf -y install cloud-init rsync
-dnf -y clean all
-
-rm -rf /var/cache /var/lib/dnf
-EORUN
-CONTAINERFILEOF
-
-LOCAL_IMAGE="localhost/bootc:test"
-podman build \
-    --retry 5 \
-    --retry-delay 5s \
-    -v "$(pwd)":/code:z \
-    -t "$LOCAL_IMAGE" \
-    -f "$CONTAINERFILE" \
-    "$BOOTC_TEMPDIR"
-
-SSH_KEY=${BOOTC_TEMPDIR}/id_rsa
+SSH_KEY=$(pwd)/target/id_rsa
+rm -vf "${SSH_KEY}"*
 ssh-keygen -f "${SSH_KEY}" -N "" -q -t rsa-sha2-256 -b 2048
+chmod 600 "${SSH_KEY}"
 
-truncate -s 10G "${BOOTC_TEMPDIR}/disk.raw"
+rm -vf target/disk.raw
+truncate -s 10G "target/disk.raw"
 
 # For test-22-logically-bound-install
 podman pull --retry 5 --retry-delay 5s quay.io/curl/curl:latest
 podman pull --retry 5 --retry-delay 5s quay.io/curl/curl-base:latest
 podman pull --retry 5 --retry-delay 5s registry.access.redhat.com/ubi9/podman:latest
+
+mkdir -p target/disks
 
 podman run \
   --rm \
@@ -107,12 +57,12 @@ podman run \
   --security-opt label=type:unconfined_t \
   -v /var/lib/containers:/var/lib/containers \
   -v /dev:/dev \
-  -v "$BOOTC_TEMPDIR":/output \
-  "$LOCAL_IMAGE" \
+  -v $(pwd)/target:/target \
+  localhost/bootc-integration \
   bootc install to-disk \
   --filesystem "xfs" \
-  --root-ssh-authorized-keys "/output/id_rsa.pub" \
+  --root-ssh-authorized-keys "/target/id_rsa.pub" \
   --karg=console=ttyS0,115200n8 \
   --generic-image \
   --via-loopback \
-  /output/disk.raw
+  /target/disk.raw
